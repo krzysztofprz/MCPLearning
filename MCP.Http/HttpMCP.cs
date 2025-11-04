f58b6f3e7c07eec50fc311db9cce05a54c34e269
Kris <krzysztofprz@gmail.com>
2025-11-04 12:07:06 +0100

    Add project files.

---
+++ MCP.Http/HttpMCP.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using ReGoMCP.Server.Endpoints;
+namespace MCP.Http
+{
+    public static class HttpMCP
+    {
+        public async static Task Configure(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Logging.AddConsole(consoleLogOptions =>
+            {
+                // Configure all logs to go to stderr
+                consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+            });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowInspector", policy =>
+                {
+                    policy.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin();
+                });
+            });
+
+            builder.Services.ConfigureHttpJsonOptions(options =>
+            {
+                options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+            });
+
+            builder.Services.AddMcpServer()
+                .WithHttpTransport()
+                .WithToolsFromAssembly();
+
+            //app.AddTodoEndpoint();
+            var app = builder.Build();
+
+            app.UseCors("AllowInspector");
+            app.MapMcp();
+
+            app.Run();
+        }
+
+        [McpServerToolType]
+        public static class EchoTool
+        {
+            [McpServerTool, Description("Echoes the message back to the client.")]
+            public static string Echo(string message) => $"hello {message}";
+        }
+    }
+}
---
+++ ReGo.Main/Program.cs
@@ -0,0 +1,5 @@
+using MCP.Http;
+using MCP.Stdio;
+
+//await StdioMCP.Configure(args);
+await HttpMCP.Configure(args);
